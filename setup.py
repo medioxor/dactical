5cd28b1fd59748978eb5e65df49870ab770dfa7d
medioxor <medioxor@gmail.com>
2025-02-02 14:27:57 +1000

    added test data

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dactical",
+    version="0.1",
+    packages=find_packages(),
+    license="",
+    author="",
+    author_email="",
+    description="Detection as Code",
+    package_data={},
+    entry_points={
+        'console_scripts': [
+        ],
+    }
+)
